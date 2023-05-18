c26da7bb1bdbb6907d039e4c349f32ae8c891ee4
Belle Hollander <bellehollander@gmail.com>
2023-05-18 09:25:13 -0500

    transient state updated

---
+++ scripts/TransientState.js
@@ -0,0 +1,26 @@
+const transientState = {
+  governorId: 0,
+  colonyId: 0,
+  facilityId: 0,
+  mineralId: 0,
+};
+
+export const setGovernorChoice = (chosenGovernor) => {
+  transientState.governorId = chosenGovernor;
+  console.log(transientState);
+};
+
+export const setColonyChoice = (chosenColony) => {
+  transientState.colonyId = chosenColony;
+  console.log(transientState);
+};
+
+export const setFacilityChoice = (chosenFacility) => {
+  transientState.facilityId = chosenFacility;
+  console.log(transientState);
+};
+
+export const setMineralChoice = (chosenMineral) => {
+  transientState.mineralId = chosenMineral;
+  console.log(transientState);
+};
