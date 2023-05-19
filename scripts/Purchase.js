2f678d7343d984d0bd6b46253effb577b25205f4
Lance Buckley <erchancy@gmail.com>
2023-05-19 10:19:55 -0500

    Fixed Purchase for the most part

--- scripts/Purchase.js
+++ scripts/Purchase.js
@@ -4,51 +4,74 @@ import { transientState } from "./TransientState.js"
 export const purchaseButtonTransfer = async (clickEvent) => {
     // Ensure a radio button is checked, 
 
-        //On purchase button click,
-         
-            const customEvent = new CustomEvent("newMineralPurchased")
-            document.dispatchEvent(customEvent)
+    //On purchase button click,
+
+    const customEvent = new CustomEvent("newMineralPurchased")
+    document.dispatchEvent(customEvent)
     const responseFacility = await fetch("http://localhost:8088/facilityInventory")
     const facilityInventories = await responseFacility.json()
     const responseColony = await fetch("http://localhost:8088/colonyInventory")
     const colonyInventories = await responseColony.json()
 
 
-            //reduce the facilityInventory amount of the checked mineral by 1 and if there were none of that mineral, create it
-            // Iterate through the inventory so as to get an actual object
+    //reduce the facilityInventory amount of the checked mineral by 1 and if there were none of that mineral, create it
+    // Iterate through the inventory so as to get an actual object
+    const matchingFacilityInventories = facilityInventories.filter(
+        (inventory) => inventory.facilityId === transientState.facilityId
+    ).filter(
+        (inventory) => inventory.mineralId === transientState.mineralId
+    )
 
-            for (const facilityInventory of facilityInventories) {
-                if (facilityInventory.mineralId === transientState.mineralId) {
-                    facilityInventory.amount--  // Walked through the debugger and this does happen but we need to post it so it becomes real
-                    for(const colonyInventory of colonyInventories) {
-                        if (colonyInventory.mineralId === transientState.mineralId) { // Perhaps we should use the .filter method here
-                            colonyInventory.amount++   //Same as above, needs to be POST to become real
-                        } else {  // This is producing thousands of objects because it pushes a new pobject to the array which then makes a new object that is pushed to the array which makes a new object...
-                                let colonyInventoryObject =
-                                {
-                                    "colonyId": transientState.colonyId,
-                                    "mineralId": transientState.mineralId,
-                                    "amount": 1
-                                }
-                                //POST the object to colonyInventory
-                                const postOptions = {
-                                    method: "POST",
-                                    headers: {
-                                        "Content-Type": "application/json"
-                                    },
-                                    body: JSON.stringify(colonyInventoryObject)
-                                }
-                                    await fetch("http://localhost:8088/colonyInventory", postOptions)
-                                }
-                            }
-                }
+    if (matchingFacilityInventories.length > 0) {
+        const facilityInventory = matchingFacilityInventories[0];
+        facilityInventory.amount--; // Walked through the debugger and this does happen but we need to post it so it becomes real
+        const postOptions = {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify(facilityInventory)
+        }
+        await fetch("http://localhost:8088/facilityInventory", postOptions)
 
-                    
-                    }
-                }
+        const matchingColonyInventories = colonyInventories.filter(
+            (inventory) => inventory.colonyId === transientState.colonyId
+        ).filter(
+            (inventory) => inventory.mineralId === transientState.mineralId
+        )
+        if (matchingColonyInventories.length > 0) {
+            const colonyInventory = matchingColonyInventories[0];
+            colonyInventory.amount++;
+            const postOptions = {
+                method: "POST",
+                headers: {
+                    "Content-Type": "application/json"
+                },
+                body: JSON.stringify(colonyInventory)
+            }
+            await fetch("http://localhost:8088/colonyInventory", postOptions)
+
+        } else {
+            const colonyInventoryObject = {
+                colonyId: transientState.colonyId,
+                mineralId: transientState.mineralId,
+                amount: 1,
+            };
+            //POST the object to colonyInventory
+            const postOptions = {
+                method: "POST",
+                headers: {
+                    "Content-Type": "application/json"
+                },
+                body: JSON.stringify(colonyInventoryObject)
+            }
+            await fetch("http://localhost:8088/colonyInventory", postOptions)
+        }
+    }
+}
 
 
-    
+
 
 
 // const postOrder = async () => {
@@ -65,7 +88,7 @@ export const purchaseButtonTransfer = async (clickEvent) => {
 document.addEventListener("click", (clickEvent) => {
     if (transientState.mineralId !== 0) {
         if (clickEvent.target.id === "purchase") {
-            purchaseButtonTransfer
+            purchaseButtonTransfer()
         }
     }
 })
